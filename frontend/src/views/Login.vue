dd74779f4a8564af5443f9fae125c0bef58169e3
aungmyatkyaw-nuos <aung.myat@globalmagicko.com>
2025-12-29 15:37:10 +0700

    fix: adjust password toggle button positioning in Login view for improved alignment

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -30,7 +30,7 @@
 							/>
 							<button
 								type="button"
-								class="absolute right-3 top-6 text-gray-500 hover:text-gray-700 focus:outline-none"
+								class="absolute right-3 top-7 text-gray-500 hover:text-gray-700 focus:outline-none"
 								@click="showPassword = !showPassword"
 							>
 								<FeatherIcon :name="showPassword ? 'eye-off' : 'eye'" class="h-5 w-5" />
